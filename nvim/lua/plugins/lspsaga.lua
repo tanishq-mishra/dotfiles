92993d6fb64f8f158fc1324820ebbdda5d581d0b
Tanishq Mishra <tanishqrmishra@gmail.com>
2025-01-18 12:20:35 +0530

    lspsaga diagnostic jump

--- nvim/lua/plugins/lspsaga.lua
+++ nvim/lua/plugins/lspsaga.lua
@@ -6,17 +6,22 @@ return {
 	},
 	config = function()
 		require("lspsaga").setup({
-      ui = {
-        code_action = ""
-      }
-         })
+			symbol_in_winbar = {
+				enable = true,
+				folder_level = 0,
+			},
+			ui = {
+				code_action = "",
+			},
+		})
 
 		local opts = { noremap = true, silent = true }
-    
-		vim.keymap.set("n", "<leader>gd", ":Lspsaga goto_definition<CR>" , opts)
-		vim.keymap.set("n", "<leader>gr", ":Lspsaga finder<CR>" , opts)
-    vim.keymap.set("n", "<leader>ca", ":Lspsaga code_action<CR>" , opts)
-    vim.keymap.set("n", "K", ":Lspsaga hover_doc<CR>" ,opts)
-    vim.keymap.set("n", "<leader>rn", ":Lspsaga rename<CR>" ,opts)
+		vim.keymap.set("n", "<leader>gd", ":Lspsaga goto_definition<CR>", opts)
+		vim.keymap.set("n", "<leader>gr", ":Lspsaga finder<CR>", opts)
+		vim.keymap.set("n", "<leader>ca", ":Lspsaga code_action<CR>", opts)
+		vim.keymap.set("n", "K", ":Lspsaga hover_doc<CR>", opts)
+		vim.keymap.set("n", "<leader>rn", ":Lspsaga rename<CR>", opts)
+		vim.keymap.set("n", "<leader>dj", ":Lspsaga diagnostic_jump_next<CR>", opts)
+		vim.keymap.set("n", "<leader>dk", ":Lspsaga diagnostic_jump_prev<CR>", opts)
 	end,
 }
