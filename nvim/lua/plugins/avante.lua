56931fce97fb199170c2b55ec65c666d64dcdb61
Tanishq Mishra <tanishqrmishra@gmail.com>
2025-02-26 12:45:41 +0530

    karabiner hyper updates, kitty color scheme tweak, avante config, tmux kitty

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -12,14 +12,14 @@ return {
 			throttle = 600,
 		},
 		auto_suggestions_provider = "openai",
-		provider = "openai",
-		openai = {
-			endpoint = "http://127.0.0.1:1234/v1",
-			model = "deepseek-r1-distill-qwen-1.5b", -- your desired model (or use gpt-4o, etc.)
-			timeout = 30000, -- timeout in milliseconds
-			temperature = 0, -- adjust if needed
-			max_tokens = 4096,
-		},
+		provider = "copilot",
+		--openai = {
+		--	endpoint = "http://127.0.0.1:1234/v1",
+		--	model = "deepseek-r1-distill-qwen-1.5b", -- your desired model (or use gpt-4o, etc.)
+		--	timeout = 30000, -- timeout in milliseconds
+		--	temperature = 0, -- adjust if needed
+		--	max_tokens = 4096,
+		--},
 		behaviour = {
 			auto_suggestions = false,
 			auto_set_highlight_group = true,
